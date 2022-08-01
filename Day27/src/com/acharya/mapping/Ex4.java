735b669dcedb1366d458d33b9739b075b33c62dc
Prajwal <Prajwal@DESKTOP-O8NF7C0>
2022-08-01 10:17:19 +0530

    First commit

---
+++ Day27/src/com/acharya/mapping/Ex4.java
@@ -0,0 +1,18 @@
+package com.acharya.mapping;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		Map<String,String> b = new HashMap<>();
+		b.put("Vishwa", "Rama");
+		b.put("vikas","abd");
+		b.put("ravi", "vk");
+		System.out.println(b);
+		System.out.println(b.get("ravi"));
+
+	}
+
+}
---
+++ Day27/src/com/acharya/mapping/Ex5.java
@@ -0,0 +1,18 @@
+package com.acharya.mapping;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+		Map<Integer,String> m = new HashMap<>();
+		  m.put(90,"Hello");
+		  m.put(47,"import");
+		  m.put(78,"abd");
+		  System.out.println(m);
+		  
+
+	}
+
+}
